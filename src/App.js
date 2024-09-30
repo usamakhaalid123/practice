d034d7c0c328addec593a5feec71bc1c07f13c5d
usama <usamamuhammadusama257@gmail.com>
2024-09-30 11:51:56 +0500

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex items-center justify-center min-h-screen'>
+      <h1 className="bg-blue-500 p-6 rounded text-center">
+      Hello world! how are you welcome to the land is
+    </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/practice.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const practice = () => {
+  return (
+    <div>practice</div>
+  )
+}
